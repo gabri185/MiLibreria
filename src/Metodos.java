4951f244b5e9506fda6bceeada45be7d078fd95d
Gabriel Domínguez <gdominguezreyes@danielcastelao.org>
2021-02-19 09:18:19 +0100

    Initial commit

---
+++ src/Metodos.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Metodos {
+
+    public void amosarCon(){
+        System.out.println("holaaa");
+
+    }
+    public void amosarVen(){
+        JOptionPane.showInputDialog("hola");
+    }
+}
